133ad5f1a32bc0457304a4a7feba7442689c863c
Hemant Kumar Yadav <hemantyadav8006@gmail.com>
2024-12-04 10:59:23 +0530

    minor fix herosection

--- src/assets/Images/imageTransfer.js
+++ src/assets/Images/imageTransfer.js
@@ -1,19 +1,21 @@
-import flexibilityImg from "./flexibilityImg.png";
-import accessibilityImg from "./accessibilityImg.png";
-import interactiveImg from "./interactiveImg.png";
-import costEffectivenessImg from "./costEffectivenessImg.png";
-import logo from "./logo.png";
-import herosection1 from "./herosection1.png";
-import herosection2 from "./herosection2.png";
-import herosection3 from "./herosection3.png";
-import herosection4 from "./herosection4.png";
-import vector from "./stream icons/vector.png"
-import vector1 from "./stream icons/vector1.png"
-import vector2 from "./stream icons/vector2.png"
-import vector3 from "./stream icons/vector3.png"
-import vector4 from "./stream icons/vector4.png"
-import vector5 from "./stream icons/vector5.png"
-import vector6 from "./stream icons/vector6.png"
+import flexibilityImg from "./HeroSection/flexibilityImg.png";
+import accessibilityImg from "./HeroSection/accessibilityImg.png";
+import interactiveImg from "./HeroSection/interactiveImg.png";
+import costEffectivenessImg from "./HeroSection/costEffectivenessImg.png";
+import logo from "./HeroSection/logo.png";
+import herosection1 from "./HeroSection/herosection1.png";
+import herosection2 from "./HeroSection/herosection2.png";
+import herosection3 from "./HeroSection/herosection3.png";
+import herosection4 from "./HeroSection/herosection4.png";
+import printerImg from "./HeroSection/printerImg.png";
+import bookshelfImg from "./HeroSection/bookshelfImg.png";
+import vector from "./stream icons/vector.png";
+import vector1 from "./stream icons/vector1.png";
+import vector2 from "./stream icons/vector2.png";
+import vector3 from "./stream icons/vector3.png";
+import vector4 from "./stream icons/vector4.png";
+import vector5 from "./stream icons/vector5.png";
+import vector6 from "./stream icons/vector6.png";
 import icon from "./TopOnlineColleges/icon.png";
 import icon1 from "./TopOnlineColleges/icon1.png";
 import icon2 from "./TopOnlineColleges/icon2.png";
@@ -23,8 +25,8 @@ import icon5 from "./TopOnlineColleges/icon5.png";
 import icon6 from "./TopOnlineColleges/icon6.png";
 import ButtonNext from "./TopOnlineColleges/ButtonNext.png";
 import ButtonPrevious from "./TopOnlineColleges/ButtonPrevious.png";
-import freeIcon from "./freeIcon.png"
-import careerBackground from "./careerBackground.png"
+import freeIcon from "./freeIcon.png";
+import careerBackground from "./careerBackground.png";
 export {
   flexibilityImg,
   accessibilityImg,
@@ -35,6 +37,8 @@ export {
   herosection2,
   herosection3,
   herosection4,
+  printerImg,
+  bookshelfImg,
   vector,
   vector1,
   vector2,
